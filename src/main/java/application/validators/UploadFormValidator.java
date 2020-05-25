9ee496e8731ef407d495747e7be10815b3607f80
Dondigidol <pillar_box@bk.ru>
2020-05-25 23:08:37 +0300

    - added video upload form validator

--- src/main/java/application/validators/UploadFormValidator.java
+++ src/main/java/application/validators/UploadFormValidator.java
@@ -4,6 +4,7 @@ import application.payload.VideoUploadRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class UploadFormValidator implements Validator {
@@ -15,19 +16,25 @@ public class UploadFormValidator implements Validator {
 
     @Override
     public void validate(Object object, Errors errors) {
-        VideoUploadRequest videoUploadRequest = (VideoUploadRequest) object;
+        VideoUploadRequest video = (VideoUploadRequest) object;
+        String title = video.getTitle();
+        String description = video.getDescription();
+        MultipartFile videoFile = video.getVideoFile();
+        MultipartFile posterFile = video.getPosterFile();
 
-        if (videoUploadRequest.getTitle() == null || videoUploadRequest.getTitle().isEmpty()){
-            errors.rejectValue("title", "title.isEmpty", "Заполните заголовок");
+        if (title.isEmpty() || title == null){
+            errors.rejectValue("title", "title.empty", "Заполните заголовок");
         }
-        if (videoUploadRequest.getDescription() == null || videoUploadRequest.getDescription().isEmpty()){
-            errors.rejectValue("description", "description.isEmpty", "Заполните описание");
+        if (description.isEmpty() || description == null){
+            errors.rejectValue("description", "description.empty", "Заполните описание");
         }
-        if (videoUploadRequest.getPosterFile() != null && videoUploadRequest.getPosterFile().isEmpty()){
-            errors.rejectValue("posterFile", "posterFile.isEmpty", "Выбирете постер");
+        if (videoFile == null || videoFile.isEmpty()){
+            errors.rejectValue("videoFile", "videoFile.empty", "Выбирете видеофайл");
         }
-        if (videoUploadRequest.getVideoFile() != null && videoUploadRequest.getVideoFile().isEmpty()){
-            errors.rejectValue("videoFile", "videoFile.isEmpty", "Выбирете видеоролик");
+        if (posterFile == null || posterFile.isEmpty()){
+            errors.rejectValue("posterFile", "posterFile.empty", "Выбирете постер");
         }
+
+
     }
 }
